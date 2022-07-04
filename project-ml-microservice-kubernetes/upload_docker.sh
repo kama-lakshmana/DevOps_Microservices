2443328f1d6fd192dcbdc26a3042d4e80e406de8
kama-lakshmana <kama.lakshmana@gmail.com>
2022-07-04 15:48:36 +0000

    project4

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=kamalakshmana/udacity
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username=kamalakshmana
+docker tag udacity $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
